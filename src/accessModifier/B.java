b9d5f206836f0b68ba0e6858331a9551ae7d9759
konicak <97625669+konicak@users.noreply.github.com>
2022-03-08 03:01:07 +0530

    all codes till class 33

---
+++ src/accessModifier/B.java
@@ -0,0 +1,12 @@
+package accessModifier;
+
+import com.java.sprintQaClass27.A;
+
+public class B extends A{
+
+	public static void main(String[] args) {
+		B obj = new B();
+	System.out.println(obj.i);
+	}
+
+}
